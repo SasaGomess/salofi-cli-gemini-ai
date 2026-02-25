d99b302fe611483dc1932ab3dfd0b0e444282d92
SasaGomes <sabrina.gomess.dev@gmail.com>
2026-02-25 15:34:49 -0300

    feat: add camada de serviço da Música

--- src/main/java/br/com/sabrinaweb/saLofi/services/MusicaService.java
+++ src/main/java/br/com/sabrinaweb/saLofi/services/MusicaService.java
@@ -1,4 +1,34 @@
 package br.com.sabrinaweb.saLofi.services;
 
+import br.com.sabrinaweb.saLofi.model.Artista;
+import br.com.sabrinaweb.saLofi.model.Musica;
+import br.com.sabrinaweb.saLofi.repository.MusicaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MusicaService {
+
+    private MusicaRepository repository;
+
+    public MusicaService(MusicaRepository repository) {
+        this.repository = repository;
+    }
+
+    public void criaMusica(String nomeMusica, String album, List<Artista> artistas){
+        Musica musica = new Musica(nomeMusica, album);
+
+        musica.setArtistas(artistas);
+
+        repository.save(musica);
+    }
+
+    public List<Musica> listarMusicas() {
+        return repository.findAll();
+    }
+
+    public List<Musica> acharMusicasPeloArtista(String nomeArtista) {
+        return repository.achaMusicaPeloArtista(nomeArtista);
+    }
 }

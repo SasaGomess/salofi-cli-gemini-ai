3bf45f3f01b493396579a2e6b2dbae13ec780e05
SasaGomes <sabrina.gomess.dev@gmail.com>
2026-02-25 15:35:19 -0300

    feat: add camada serviço Artista

--- src/main/java/br/com/sabrinaweb/saLofi/services/ArtistaService.java
+++ src/main/java/br/com/sabrinaweb/saLofi/services/ArtistaService.java
@@ -1,4 +1,22 @@
 package br.com.sabrinaweb.saLofi.services;
 
+import br.com.sabrinaweb.saLofi.model.Artista;
+import br.com.sabrinaweb.saLofi.repository.ArtistaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class ArtistaService {
+
+    private ArtistaRepository repository;
+
+    public void criaArtista(String nome, String tipo) {
+        Artista artista = new Artista(nome, tipo);
+        repository.save(artista);
+    }
+
+    public Artista achaArtista(String nomeArtista) {
+        return repository.findByNomeContainingIgnoreCase(nomeArtista).orElseThrow(() -> new IllegalArgumentException("Artista não encontrado"));
+    }
 }

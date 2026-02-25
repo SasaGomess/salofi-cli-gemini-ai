9bd5ab9baa5f1050f321841f6083f22dc525c561
SasaGomes <sabrina.gomess.dev@gmail.com>
2026-02-25 15:37:42 -0300

    feat: add métodos de consulta personalizados com JPQL e derived uerys nos repositórios de música e artista

--- src/main/java/br/com/sabrinaweb/saLofi/repository/MusicaRepository.java
+++ src/main/java/br/com/sabrinaweb/saLofi/repository/MusicaRepository.java
@@ -1,4 +1,14 @@
 package br.com.sabrinaweb.saLofi.repository;
 
-public interface MusicaRepository {
+import br.com.sabrinaweb.saLofi.model.Artista;
+import br.com.sabrinaweb.saLofi.model.Musica;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface MusicaRepository extends JpaRepository<Musica, Long> {
+    @Query("SELECT m FROM Musica m JOIN m.artistas a WHERE a.nome ILIKE :nomeArtista")
+    List<Musica> achaMusicaPeloArtista(@Param("nomeArtista") String nomeArtista);
 }

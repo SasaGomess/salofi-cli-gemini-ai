835159273cfc065a882fecbde160438a80f8e907
SasaGomes <sabrina.gomess.dev@gmail.com>
2026-02-25 15:36:05 -0300

    feat: add camada de serviço da API do Gemini

--- src/main/java/br/com/sabrinaweb/saLofi/services/GeminiChatService.java
+++ src/main/java/br/com/sabrinaweb/saLofi/services/GeminiChatService.java
@@ -1,4 +1,22 @@
 package br.com.sabrinaweb.saLofi.services;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GeminiChatService {
+
+    private final ChatClient client;
+
+    public GeminiChatService(ChatClient.Builder clientBuilder){
+        this.client = clientBuilder.build();
+    }
+
+    public String chat(String message) {
+
+        return this.client.prompt()
+                .user(message)
+                .call()
+                .content();
+    }
 }
